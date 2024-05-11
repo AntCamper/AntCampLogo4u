8c2f17295d77e80494aaba0a9777bcc528f5706a
AntCamper <152764140+AntCamper@users.noreply.github.com>
2024-05-10 21:09:45 -0700

    copied over files from miniature-eureka

--- AntCampNoteTaker/Develop/routes/indexroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const notesRouter = require('Develop\routes\notes.js');
-
-const router = express.Router();
-
-router.use("/notes", notes);
-
-module.exports = router;
